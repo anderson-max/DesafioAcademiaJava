184bb47ae839bbdfde89f62791178b73a8969442
DESKTOP-E1N116N\andre <anderson.santana.27@hotmail.com>
2022-02-19 15:16:15 -0300

    Atividades....

---
+++ src/com/anderson/Atividade01.java
@@ -0,0 +1,12 @@
+package com.anderson;
+
+public class Atividade01 {
+
+	public static void main(String[] args) {
+	    int n = 6;
+	    for (int i = 0; i < n; i++) {
+		     System.out.println( "*".repeat(i + 1));
+
+			}
+	  }
+	}
